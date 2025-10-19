375106883095ee52251f97e514d86c45f12ba6b6
Drex <nicknamedrex@gmail.com>
2025-10-19 12:18:06 +0200

    Use empty slab states

--- src/main/java/me/drex/terraformpatch/block/type/SlabPolymerBlock.java
+++ src/main/java/me/drex/terraformpatch/block/type/SlabPolymerBlock.java
@@ -4,12 +4,14 @@ import eu.pb4.factorytools.api.block.model.generic.BSMMParticleBlock;
 import eu.pb4.factorytools.api.block.FactoryBlock;
 import eu.pb4.factorytools.api.block.model.generic.ShiftyBlockStateModel;
 import eu.pb4.polymer.blocks.api.BlockModelType;
+import eu.pb4.polymer.blocks.api.PolymerBlockResourceUtils;
 import eu.pb4.polymer.blocks.api.PolymerTexturedBlock;
 import eu.pb4.polymer.virtualentity.api.ElementHolder;
 import me.drex.terraformpatch.TerraformerPatch;
 import me.drex.terraformpatch.block.PolymerBlockHelper;
 import me.drex.terraformpatch.res.ResourcePackGenerator;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.jetbrains.annotations.Nullable;
 import xyz.nucleoid.packettweaker.PacketContext;
 
@@ -26,14 +28,26 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.SlabType;
 import net.minecraft.world.phys.BlockHitResult;
 
-public record SlabPolymerBlock(BlockState bottomState, BlockState bottomStateWaterlogged) implements FactoryBlock, PolymerTexturedBlock, BSMMParticleBlock {
+public record SlabPolymerBlock(BlockState bottomState,
+                               BlockState bottomStateWaterlogged) implements FactoryBlock, PolymerTexturedBlock, BSMMParticleBlock {
+
+    private static final BlockState[] EMPTY_STATES = new BlockState[4];
+
+    static {
+        int i = 0;
+        for (boolean top : new boolean[]{false, true}) {
+            for (boolean waterlogged : new boolean[]{false, true}) {
+                EMPTY_STATES[i] = PolymerBlockResourceUtils.requestEmpty(BlockModelType.getSlab(top, waterlogged));
+                i++;
+            }
+        }
+    }
+
     public static SlabPolymerBlock of(ResourceLocation id) {
         try {
             BlockState bottom = PolymerBlockHelper.requestPolymerBlockState(id, "type=bottom", BlockModelType.SCULK_SENSOR_BLOCK);
             BlockState bottomWaterlogged = PolymerBlockHelper.requestPolymerBlockState(id, "type=bottom", BlockModelType.SCULK_SENSOR_BLOCK_WATERLOGGED);
-            var block = new SlabPolymerBlock(bottom, bottomWaterlogged);
-            ResourcePackGenerator.expandBlockModel(id, block, s -> !s.equals("type=bottom") || bottom == null || bottomWaterlogged == null);
-            return block;
+            return new SlabPolymerBlock(bottom, bottomWaterlogged);
         } catch (IOException e) {
             TerraformerPatch.LOGGER.error("Failed to handle slab block {}", id, e);
             return null;
@@ -43,14 +57,21 @@ public record SlabPolymerBlock(BlockState bottomState, BlockState bottomStateWat
     @Override
     public BlockState getPolymerBlockState(BlockState state, PacketContext context) {
         SlabType slabType = state.getValue(SlabBlock.TYPE);
+        Boolean waterlogged = state.getValue(SlabBlock.WATERLOGGED);
         if (slabType == SlabType.BOTTOM) {
-            var vanillaState = state.getValue(SlabBlock.WATERLOGGED) ? bottomStateWaterlogged : bottomState;
+            var vanillaState = waterlogged ? bottomStateWaterlogged : bottomState;
             if (vanillaState != null) {
                 return vanillaState;
             }
         }
 
-        return slabType != SlabType.DOUBLE ? Blocks.SANDSTONE_SLAB.withPropertiesOf(state) : Blocks.BARRIER.defaultBlockState();
+        if (slabType == SlabType.DOUBLE) {
+            return Blocks.BARRIER.defaultBlockState().setValue(BlockStateProperties.WATERLOGGED, waterlogged);
+        } else {
+            int i = slabType == SlabType.BOTTOM ? 0 : 2;
+            if (waterlogged) i++;
+            return EMPTY_STATES[i];
+        }
     }
 
     @Override
